383e0f436c23576be5f2737137a349a0d4a9c3e5
marleysudbury <marleysudbury@gmail.com>
2021-02-20 00:52:05 +0000

    More changes to leaderboard class, nearly there!

--- leaderboard.py
+++ leaderboard.py
@@ -9,26 +9,41 @@ class Leaderboard:
             "concentric": ("???", 000)
         }
 
+    def create_file(self):
+        pass
+
+    def draw(self):
+        easy = self.font.render('{:<12}{:<10}{}'.format(
+            'Easy', self.leaderboard[0][0], self.leaderboard[0][1]), True, (255, 0, 0))
+        medium = self.font.render(
+            '{:<12}{:<10}{}'.format('Medium', self.leaderboard[1][0], self.leaderboard[1][1]), True, (255, 0, 0))
+        hard = self.font.render('{:<12}{:<10}{}'.format(
+            'Hard', self.leaderboard[2][0], self.leaderboard[2][1]), True, (255, 0, 0))
+        concentric = self.font.render(
+            '{:<12}{}{:>10}'.format('Concentric', self.leaderboard[3][0], ' stage ' + self.leaderboard[3][1]), True, (255, 0, 0))
+        self.game_display.blit(easy, (50, 90))
+        self.game_display.blit(medium, (50, 120))
+        self.game_display.blit(hard, (50, 150))
+        self.game_display.blit(concentric, (50, 180))
+
     def load(self):
-        self.leaderboard = {}
+        # self.leaderboard = {}
         lb_file = open('data/leader.txt')
         text = lb_file.read().split('\n')
-        for i in range(0, len(text)):
-            self.leaderboard[i] = text[i].split(',')
+        # for i in range(0, len(text)):
+        for i, mode in enumerate(self.leaderboard):
+            mode = tuple(text[i].split(','))
 
         lb_file.close()
 
-    def create_file(self):
+    def reset(self):
         pass
 
     def save(self):
         text = ''
-        for i in range(0, 4):
-            text += '{},{}\n'.format(self.leaderboard[i][0],
-                                     self.leaderboard[i][1])
+        # for i in range(0, 4):
+        for mode in self.leaderboard:
+            text += '{},{}\n'.format(mode[0], mode[1])
         lb_file = open('data/leader.txt', 'w')
         lb_file.write(text)
         lb_file.close()
-
-    def draw(self):
-        pass
